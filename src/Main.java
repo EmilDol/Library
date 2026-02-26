3561dc34b3acd95db322440eadf7ced55543bc2a
Emil Dolchinkov <emildolchinkov@gmail.com>
2026-02-26 23:12:57 +0200

    Testing the powershell cli

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import  java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+void main() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Welcome to My CLI App!");
+    System.out.println("Type something (or 'exit' to quit):");
+
+    while (true) {
+        System.out.print("> ");
+        String input = scanner.nextLine();
+
+        if (input.equalsIgnoreCase("exit")) {
+            System.out.println("Goodbye!");
+            break;
+        }
+
+        System.out.println("You typed: " + input);
     }
+
+    scanner.close();
 }
